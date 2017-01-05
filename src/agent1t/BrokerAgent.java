78ea2e0a28e82d802a500b99c428adb183ac59cf
Beket <jackatwork@yandex.ru>
2017-01-05 17:53:16 +0300

    Before adding additional agent

---
+++ src/agent1t/BrokerAgent.java
@@ -0,0 +1,38 @@
+package agent1t;
+
+//package 
+
+import jade.core.Agent;
+import jade.core.AID;
+
+//import jade.core.Agent;
+import jade.core.behaviours.*;
+import java.util.*;
+import java.util.List;
+import jade.core.Agent;
+import jade.core.AID;
+import jade.core.behaviours.*;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+
+import jade.core.behaviours.SimpleBehaviour;
+import jade.lang.acl.ACLMessage;
+
+
+/**
+@author Ponomarev Sergey
+*/
+
+
+
+public class BrokerAgent {
+
+	
+	
+	
+	
+}
